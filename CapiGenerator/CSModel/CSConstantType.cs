4c482f24bb2f9c851b9fe162f36ac3b9629e93f9
Emil Vad <emilsvad@gmail.com>
2024-02-11 11:49:50 +0100

    wip

---
+++ CapiGenerator/CSModel/CSConstantType.cs
@@ -0,0 +1,24 @@
+namespace CapiGenerator.CSModel;
+
+public enum CSConstantType
+{
+    Unknown = -1,
+    NONE = 0,
+    Bool,
+    Byte,
+    Sbyte,
+    Char,
+    Decimal,
+    Double,
+    Float,
+    Int,
+    Uint,
+    Nint,
+    Nuint,
+    Long,
+    Ulong,
+    Short,
+    Ushort,
+    String,
+    Object,
+}
--- CapiGenerator/CSModel/ConstantToken/CSConstLiteralToken.cs
+++ CapiGenerator/CSModel/ConstantToken/CSConstLiteralToken.cs
@@ -1,32 +1,10 @@
 using System.Globalization;
-namespace CapiGenerator.CModel.ConstantToken;
+namespace CapiGenerator.CSModel.ConstantToken;
 
-public class CConstLiteralToken : BaseCConstantToken
+public class CSConstLiteralToken(string value, CSConstantType type) : BaseCSConstantToken
 {
-    private readonly CConstantType _type;
-
-    public string Value { get; }
-    public CConstantType Type => _type;
-
-    // public override string GetOutValue()
-    // {
-    //     return Type switch
-    //     {
-    //         ConstantType.String => $"\"{Value[1..^1]}\"u8",
-    //         ConstantType.Char => $"(byte){Value}",
-    //         _ => Value,
-    //     };
-    // }
-
-    public CConstLiteralToken(string value)
-    {
-        Value = value;
-        _type = GetConstantType(value, out var newValue);
-        if (newValue != null)
-        {
-            Value = newValue;
-        }
-    }
+    public string Value = value;
+    public CSConstantType Type => type;
 
     public bool TryParseValueAsFloat(out double value)
     {
@@ -37,58 +15,5 @@ public class CConstLiteralToken : BaseCConstantToken
     public bool TryParseValueAsInteger(out long value)
     {
         return long.TryParse(Value, NumberStyles.Integer | NumberStyles.HexNumber, null, out value);
-    }
-
-    private static CConstantType GetConstantType(string value, out string? newValue)
-    {
-        value = value.Trim();
-        newValue = value;
-
-        if (value.StartsWith('"') ||
-            value.EndsWith('"'))
-        {
-            return CConstantType.String;
-        }
-
-        if (value.StartsWith('\'') ||
-            value.EndsWith('\''))
-        {
-            return CConstantType.Char;
-        }
-
-        bool isHex = value.StartsWith("0x", StringComparison.OrdinalIgnoreCase);
-        bool isFloat = value.Contains('.') || value.Contains('e') || value.Contains('E');
-        bool isOctal = value.StartsWith("0", StringComparison.OrdinalIgnoreCase) && !isHex;
-
-        if (isHex && long.TryParse(value[2..], NumberStyles.HexNumber, CultureInfo.InvariantCulture, out _))
-        {
-            return CConstantType.Int;
-        }
-
-        if (isFloat && double.TryParse(value, NumberStyles.Float, CultureInfo.InvariantCulture, out _))
-        {
-            return CConstantType.Float;
-        }
-
-        if (isOctal)
-        {
-            try
-            {
-                long octalAsLong = Convert.ToInt64(value[1..], 8);
-                newValue = octalAsLong.ToString(CultureInfo.InvariantCulture);
-                return CConstantType.Int;
-            }
-            catch (Exception)
-            {
-                // ignored
-            }
-        }
-
-        if (long.TryParse(value, NumberStyles.Integer, CultureInfo.InvariantCulture, out _))
-        {
-            return CConstantType.Int;
-        }
-
-        return CConstantType.Unknown;
     }
 }

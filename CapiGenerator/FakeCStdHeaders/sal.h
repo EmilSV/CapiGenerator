4458a8d0e3af3e7f56eb9b1db22c15f0957f75a7
Emil Vad <emilsvad@gmail.com>
2026-08-20 19:56:50 +0200

    added more headers

---
+++ CapiGenerator/FakeCStdHeaders/sal.h
@@ -0,0 +1,13 @@
+#ifndef CAPI_GENERATOR_FAKE_C_STD_HEADERS_SAL_H
+#define CAPI_GENERATOR_FAKE_C_STD_HEADERS_SAL_H
+
+#define _In_bytecount_(x)
+#define _Inout_z_cap_(x)
+#define _Out_z_cap_(x)
+#define _Out_cap_(x)
+#define _Out_bytecap_(x)
+#define _Out_z_bytecap_(x)
+#define _Printf_format_string_
+#define _Scanf_format_string_impl_
+
+#endif // CAPI_GENERATOR_FAKE_C_STD_HEADERS_SAL_H
